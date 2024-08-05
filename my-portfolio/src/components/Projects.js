7d705eaee0176d17dcba9669b05c342a19fa6c7f
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-08-05 15:41:12 -0400

    initial project section setup

---
+++ my-portfolio/src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <p>Details about projects...</p>
+      {/* Add more details here */}
+    </div>
+  );
+};
+
+export default Projects;
