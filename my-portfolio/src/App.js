a452f991fd6f51091ccccbaf5dd970da52ddc8d7
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-08-05 16:07:54 -0400

    setup routing to the components setup previously

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import About from './components/About';
+import WorkExperience from './components/WorkExperience';
+import Skills from './components/Skills';
+import Education from './components/Education';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to My Portfolio</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/work-exp" element={<WorkExperience />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
