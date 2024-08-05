5d3e6a875ad3ced7a5c4d2200c2b73d066aeabf8
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-08-05 15:39:27 -0400

    setup initial education section

---
+++ my-portfolio/src/components/Education.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Education = () => {
+  return (
+    <div>
+      <h2>Education</h2>
+      <p>Details about education...</p>
+    </div>
+  );
+};
+
+export default Education;
