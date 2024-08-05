febf045a05d16e1ae43ac5f0570369333a47ab47
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-08-05 15:36:41 -0400

    setup the initial about section

---
+++ my-portfolio/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>Hi, I am Akshay Kumar...</p>
+    </div>
+  );
+};
+
+export default About;
