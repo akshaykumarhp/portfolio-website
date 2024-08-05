71c3513e75a34edef254b3692d30463d053ce18f
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-08-05 15:40:10 -0400

    initial skills section setup

---
+++ my-portfolio/src/components/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div>
+      <h2>My Skillset</h2>
+      <p>Details about skills...</p>
+    </div>
+  );
+};
+
+export default Skills;
