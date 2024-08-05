e1ea8b5bbfaace8395451a7c05cd7af3758a72e0
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-08-05 15:38:27 -0400

    initial setup for work experience setup

---
+++ my-portfolio/src/components/WorkExperience.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WorkExperience = () => {
+  return (
+    <div>
+      <h2>Work Experience</h2>
+      <p>Details about work experience...</p>
+    </div>
+  );
+};
+
+export default WorkExperience;
