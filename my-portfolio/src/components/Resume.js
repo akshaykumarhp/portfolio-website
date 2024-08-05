749ba338ad65f8105e2a30946760c4dcb9f71bd7
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-08-05 15:41:48 -0400

    initial resume section setup

---
+++ my-portfolio/src/components/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div>
+      <h2>Resume</h2>
+      <p>Details about resume...</p>
+      {/* Add more details here */}
+    </div>
+  );
+};
+
+export default Resume;
