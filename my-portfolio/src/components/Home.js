b4dc8ef5e4e072c156b00e1923bcaa9829b40394
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-08-13 10:28:56 -0400

    added social icons

--- my-portfolio/src/components/Home.js
+++ my-portfolio/src/components/Home.js
@@ -7,10 +7,15 @@ const Home = () => {
       <h1>Hi, I am <span>Akshay Kumar</span></h1>
       <p>Software Engineer | Full-stack Developer | ML Enthusiast</p>
       <div className="social-links">
-        <a href="https://github.com/akshaykumarhp" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="https://www.linkedin.com/in/akshaykumarhp/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="https://www.instagram.com/akshaykumarhp" target="_blank" rel="noopener noreferrer">Instagram</a>
-        <a href="https://x.com/akshayhp11" target="_blank" rel="noopener noreferrer">X</a>
+        <a href="https://github.com/akshaykumarhp" target="_blank" rel="noopener noreferrer">
+          <img src="/githublogo.png" alt="GitHub" className="social-icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/akshaykumarhp/" target="_blank" rel="noopener noreferrer">
+          <img src="/linkedinlogo.png" alt="LinkedIn" className="social-icon" />
+        </a>
+        <a href="https://x.com/akshayhp11" target="_blank" rel="noopener noreferrer">
+          <img src="/xlogo.avif" alt="X" className="social-icon" />
+        </a>
       </div>
     </div>
   );
